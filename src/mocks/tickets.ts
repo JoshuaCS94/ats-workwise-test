d0a81e9e79e86826a584ced5d83c963d7b6b51fa
Josué Cortina <joshuacs.main@gmail.com>
2022-08-30 11:39:00 -0400

    refactor: Move all mock data into `mocks` folder

---
+++ src/mocks/tickets.ts
@@ -0,0 +1,24 @@
+import { Ticket } from '../models'
+
+export const TEST_TICKETS: Ticket[] = [
+  { id: 0, title: 'Ticket 1', description: 'Description 1', assigneeId: 0, columnId: 0 },
+  { id: 1, title: 'Ticket 2', description: 'Description 2', assigneeId: 0, columnId: 1 },
+  { id: 2, title: 'Ticket 3', description: 'Description 3', assigneeId: 1, columnId: 2 },
+  { id: 3, title: 'Ticket 4', description: 'Description 4', columnId: 3 },
+  { id: 4, title: 'Ticket 5', description: 'Description 5', columnId: 1 },
+  { id: 5, title: 'Ticket 6', description: 'Description 6', assigneeId: 0, columnId: 1 },
+  { id: 6, title: 'Ticket 7', description: 'Description 7', assigneeId: 2, columnId: 2 },
+  { id: 7, title: 'Ticket 8', description: 'Description 8', assigneeId: 1, columnId: 0 },
+  { id: 8, title: 'Ticket 9', description: 'Description 9', columnId: 3 },
+  { id: 9, title: 'Ticket 10', description: 'Description 10', assigneeId: 1, columnId: 1 },
+  { id: 10, title: 'Ticket 11', description: 'Description 11', assigneeId: 2, columnId: 1 },
+  { id: 11, title: 'Ticket 12', description: 'Description 12', assigneeId: 2, columnId: 1 },
+  { id: 12, title: 'Ticket 13', description: 'Description 13', columnId: 1 },
+  { id: 13, title: 'Ticket 14', description: 'Description 14', assigneeId: 1, columnId: 0 },
+  { id: 14, title: 'Ticket 15', description: 'Description 15', assigneeId: 0, columnId: 0 },
+  { id: 15, title: 'Ticket 16', description: 'Description 16', columnId: 2 },
+  { id: 16, title: 'Ticket 17', description: 'Description 17', assigneeId: 1, columnId: 3 },
+  { id: 17, title: 'Ticket 18', description: 'Description 18', columnId: 1 },
+  { id: 18, title: 'Ticket 19', description: 'Description 19', assigneeId: 0, columnId: 1 },
+  { id: 19, title: 'Ticket 20', description: 'Description 20', assigneeId: 1, columnId: 1 },
+]
---
+++ src/mocks/users.ts
@@ -0,0 +1,7 @@
+import { User } from '../models'
+
+export const TEST_USERS: User[] = [
+  { id: 0, name: 'User 1' },
+  { id: 1, name: 'User 2' },
+  { id: 2, name: 'User 3' },
+]
