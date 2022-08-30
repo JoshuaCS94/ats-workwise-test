16a3f0ce3784be6024c25ffec07d1be6b4f764a5
Josué Cortina <joshuacs.main@gmail.com>
2022-08-30 10:31:00 -0400

    build: Install and setup `DaisyUI`

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/{pages,components}/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/{pages,components}/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 }
